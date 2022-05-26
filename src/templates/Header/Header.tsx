9988c52eb752bdd11889e29539f50c3dcd9ccbe9
m-Murakami1313 <mituvv@gmail.com>
2022-05-27 08:48:23 +0900

    add Headerにボタンを追加

--- src/templates/Header/Header.tsx
+++ src/templates/Header/Header.tsx
@@ -5,23 +5,27 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 
 import styles from "./Header.module.scss";
+import { Button } from "@mui/material";
 
 export const Header = () => {
   return (
     <div className={styles.root}>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
+      <Box sx={{ flexGrow: 1 }} className={styles.box}>
+        <AppBar position="fixed" className={styles.appbar} color="default">
+          <Toolbar className={styles.toolBar}>
+            {/* <IconButton
               size="large"
               edge="start"
-              color="inherit"
               aria-label="menu"
               sx={{ mr: 2 }}
-            ></IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
+            ></IconButton> */}
+            <Button size="large">News</Button>
+            <div className={styles.buttonsContainer}>
+              <Button className={styles.button}>About</Button>
+              <Button className={styles.button}>Works</Button>
+              <Button className={styles.button}>Traning</Button>
+              <Button className={styles.button}>SkillSets</Button>
+            </div>
           </Toolbar>
         </AppBar>
       </Box>
