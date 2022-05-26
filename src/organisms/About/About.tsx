d6b4a83988c5bd78376bd0071b01d62e7e79ee40
m-Murakami1313 <mituvv@gmail.com>
2022-05-27 07:01:55 +0900

    add Aboutを仮で作成

---
+++ src/organisms/About/About.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/system";
+
+import styles from "./About.module.scss";
+
+export const About = () => {
+  return (
+    <div>
+      <Box className={styles.about}>
+        <h1>about</h1>
+      </Box>
+    </div>
+  );
+};
