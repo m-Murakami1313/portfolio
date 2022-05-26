8a562ede8bb2fc6c4474d2387447f59513d94d1a
m-Murakami1313 <mituvv@gmail.com>
2022-05-27 07:05:29 +0900

    remove 不要なコードの削除

--- src/pages/Top/Top.tsx
+++ src/pages/Top/Top.tsx
@@ -1,5 +1,3 @@
-import { Container } from "@mui/system";
-import React from "react";
 import { Body } from "../../templates/Body/Body";
 import { Header } from "../../templates/Header/Header";
 
@@ -7,10 +5,9 @@ import styles from "./Top.module.scss";
 
 export const Top = () => {
   return (
-      <Container className={styles.top}>
-        <Header />
-        <Body />
-        <p>mailform</p>
-      </Container>
+    <div>
+      <Header />
+      <Body />
+    </div>
   );
 };
