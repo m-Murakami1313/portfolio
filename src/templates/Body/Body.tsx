93ea980a3b7d183eb304e1c0c83abff7f8d9c27d
m-Murakami1313 <mituvv@gmail.com>
2022-05-27 07:05:03 +0900

    add 必要な項目をインポートした

--- src/templates/Body/Body.tsx
+++ src/templates/Body/Body.tsx
@@ -1,13 +1,19 @@
 import styles from "./Body.module.scss";
 import { Works } from "../../organisms/Works/Works";
+import { About } from "../../organisms/About/About";
+import { Training } from "../../organisms/Training/Training";
+import { SkillSet } from "../../organisms/SkillSet/SkillSet";
+import { Container } from "@mui/system";
 
 export const Body = () => {
   return (
     <div className={styles.body}>
-      <p>ABOUT</p>
-      <Works />
-      <p>training</p>
-      <p>SKILLSET</p>
+      <Container>
+        <About />
+        <Works />
+        <Training />
+        <SkillSet />
+      </Container>
     </div>
   );
 };
