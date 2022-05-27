b8eeb4929409c5252ab7d271a173a3748b751ead
m-Murakami1313 <mituvv@gmail.com>
2022-05-27 16:55:45 +0900

    add Headerに画面スクロールを出来るボタンを作成

--- src/templates/Header/Header.tsx
+++ src/templates/Header/Header.tsx
@@ -3,14 +3,18 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
+import { Button } from "@mui/material";
+import { Link as Scroll } from "react-scroll";
 
 import styles from "./Header.module.scss";
-import { Button } from "@mui/material";
+import { useSize } from "../../hooks/UseSize";
 
 export const Header = () => {
+  const { isMobileSize } = useSize();
+
   return (
     <div className={styles.root}>
-      <Box sx={{ flexGrow: 1 }} className={styles.box}>
+      <Box sx={{ flexGrow: 1 }} id="header" className={styles.box}>
         <AppBar position="fixed" className={styles.appbar} color="default">
           <Toolbar className={styles.toolBar}>
             {/* <IconButton
@@ -19,13 +23,41 @@ export const Header = () => {
               aria-label="menu"
               sx={{ mr: 2 }}
             ></IconButton> */}
-            <Button size="large">News</Button>
-            <div className={styles.buttonsContainer}>
-              <Button className={styles.button}>About</Button>
-              <Button className={styles.button}>Works</Button>
-              <Button className={styles.button}>Traning</Button>
-              <Button className={styles.button}>SkillSets</Button>
-            </div>
+            <Button size="large">
+              <nav>
+                <Scroll to="header" smooth={true}>
+                  News
+                </Scroll>
+              </nav>
+            </Button>
+            {isMobileSize ? (
+              <div className={styles.buttonsContainer}>
+                <Button className={styles.button}>
+                  <nav>
+                    <Scroll to="about" smooth={true}>
+                      About
+                    </Scroll>
+                  </nav>
+                </Button>
+                <Button className={styles.button}>
+                  <Scroll to="works" smooth={true}>
+                    Works
+                  </Scroll>
+                </Button>
+                <Button className={styles.button}>
+                  <Scroll to="training" smooth={true}>
+                    Traning
+                  </Scroll>
+                </Button>
+                <Button className={styles.button}>
+                  <Scroll to="skillSet" smooth={true}>
+                    SkillSet
+                  </Scroll>
+                </Button>
+              </div>
+            ) : (
+              <p>1</p>
+            )}
           </Toolbar>
         </AppBar>
       </Box>
