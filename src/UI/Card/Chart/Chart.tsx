e772fe96dd85c5d0b86497c226e10399ad70c311
m-Murakami1313 <mituvv@gmail.com>
2022-05-29 18:38:28 +0900

    add skillsetをrechartを使用し、仮で作成した

---
+++ src/UI/Card/Chart/Chart.tsx
@@ -0,0 +1,74 @@
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+import styles from "./Chart.module.scss";
+
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+export const Chart = () => {
+  return (
+    <div className={styles.chart}>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart width={400} height={200} layout="vertical" data={data}>
+          <XAxis type="number" />
+          <YAxis dataKey="name" type="category" />
+          <Bar dataKey="pv" fill="#8884d8" />
+          <Bar dataKey="uv" fill="#82ca9d" />
+          <Bar dataKey="amt" fill="#74ca1d" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
--- src/organisms/SkillSet/SkillSet.tsx
+++ src/organisms/SkillSet/SkillSet.tsx
@@ -1,9 +1,11 @@
+import { Chart } from "../../UI/Card/Chart/Chart";
 import styles from "./SkillSet.module.scss";
 
 export const SkillSet = () => {
   return (
     <div id="skillSet">
       <h1 className={styles.h1}>SkillSet</h1>
+      <Chart />
     </div>
   );
 };
