3ba8b54169e810b14b1eafc37c5753c445f59692
m-Murakami1313 <mituvv@gmail.com>
2022-05-31 23:27:41 +0900

    add skillを記載できるようにカードを仮で設定した

--- src/organisms/SkillSet/SkillSet.tsx
+++ src/organisms/SkillSet/SkillSet.tsx
@@ -1,10 +1,47 @@
+import { Grid } from "@mui/material";
+
+import { CardUI } from "../../UI/Card/CardUI";
 import { Chart } from "../../UI/Chart/Chart";
 import styles from "./SkillSet.module.scss";
 
 export const SkillSet = () => {
+  const firstContents = {
+    title: "a",
+    subheader: "b",
+    content: "content",
+  };
+
   return (
     <div id="skillSet">
       <h1 className={styles.h1}>SkillSet</h1>
+      <Grid
+        container
+        className={styles.container}
+        spacing={4}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        <Grid item>
+          <CardUI
+            title={firstContents.title}
+            subheader={firstContents.subheader}
+            content={firstContents.content}
+          />
+        </Grid>
+        <Grid item>
+          <CardUI
+            title={firstContents.title}
+            subheader={firstContents.subheader}
+            content={firstContents.content}
+          />
+        </Grid>
+        <Grid item>
+          <CardUI
+            title={firstContents.title}
+            subheader={firstContents.subheader}
+            content={firstContents.content}
+          />
+        </Grid>
+      </Grid>
       <Chart />
     </div>
   );
