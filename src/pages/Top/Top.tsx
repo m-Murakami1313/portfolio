21bb009b1987d1786c97d01dc4811a22275946a0
m-Murakami1313 <mituvv@gmail.com>
2022-06-05 17:54:35 +0900

    add Homeファイルをimportした

--- src/pages/Top/Top.tsx
+++ src/pages/Top/Top.tsx
@@ -1,6 +1,7 @@
 import { createTheme, ThemeProvider } from "@mui/material";
 import { ThemeOptions } from "@mui/material";
 
+import { Home } from "../../organisms/Home/Home";
 import { Body } from "../../templates/Body/Body";
 import { Header } from "../../templates/Header/Header";
 import styles from "./Top.module.scss";
@@ -8,6 +9,7 @@ import styles from "./Top.module.scss";
 export const Top = () => {
   return (
     <div className={styles.top}>
+      <Home />
       <Header />
       <Body />
     </div>
