ef6031164fc322c0de7a8c28907147734885300a
m-Murakami1313 <mituvv@gmail.com>
2022-09-12 18:37:57 +0900

    add　レシピ作成アプリの画像を追加した

--- src/libs/worksContents.ts
+++ src/libs/worksContents.ts
@@ -1,17 +1,18 @@
 import portfolioImage from "../images/portfolio.png";
+import recipeListImage from "../images/recipeList.png";
 
 export const worksContents = [
   {
     title: "レシピ作成アプリ",
-    // image: recipeListImage,
-    content: "使用言語:Next.js,typescript,tailwind,prisma,docker",
+    image: recipeListImage,
+    content: "使用技術:Next.js,typescript,tailwind,prisma,docker",
     deploy:"デプロイ先:Vercel,supabase(postgres)",
-    url: "https://recipe-list-jwuz98v31-m-murakami1313.vercel.app/",
+    url: "https://recipe-list-bbm5rhmzn-m-murakami1313.vercel.app/",
   },
   {
     title: "ポートフォリオ",
     image: portfolioImage,
-    content: "使用言語:React,typescript,docker",
+    content: "使用技術:React,typescript,docker",
     deploy:"デプロイ先:GithubPages",
     url: "https://m-murakami1313.github.io/portfolio/",
   },
