1b58f67652901feba650c1924c8a04e5ae15797c
m-Murakami1313 <mituvv@gmail.com>
2022-09-21 23:22:26 +0900

    update prefecturesChartsをアップデートした

--- src/libs/traningsContents.ts
+++ src/libs/traningsContents.ts
@@ -2,15 +2,14 @@ import covidImage from "../images/covidtracker.png";
 import prefecturesImage from "../images/prefecturesChart.png";
 import movieListImage from "../images/movieList.png";
 
-
 export const trainingList = [
   {
     imgsrc: prefecturesImage,
-    url: "https://m-murakami1313.github.io/PrefecturesCharts/",
+    url: "https://prefectures-chart-4bx1abrme-m-murakami1313.vercel.app/",
     alt: "prefectures",
     title: "都道府県別人口増減数表",
     text: "外部APIからデータを取得し、折れ線グラフを表示する",
-    codingLanguage: "使用言語 React/Typescript",
+    codingLanguage: "使用技術 Nextjs/Typescript Docker GithubActions",
   },
   {
     imgsrc: covidImage,
